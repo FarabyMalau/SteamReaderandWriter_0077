b010b8c355c13b8ebe82783eda1570571e0a91af
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-10 14:54:52 +0600

    Membuat int main (string baris)

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main() {
+    string baris;
+
+    //membuka file dalam mode menulis 
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("data.txt"); 
+}
