2265e35053c235ba96da57ddfba99a1d1e747156
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-10 15:09:04 +0600

    Menambahkan proses try awal program

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -4,3 +4,15 @@
 #include <array>
 // untuk obyek array yang akan kita gunakan
 using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; // penanda 1:...
+    try {
+        array<int, 3> data = { 10, 20, 30 };
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array ke elemen 5
+
+    }
+}
