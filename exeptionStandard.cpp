36cf5df77030556f9a2c5cd4f61a06e62fc1594b
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-10 15:11:50 +0600

    Menambahkan catch Baris program yang terakhir

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -13,6 +13,13 @@ int main()
         //pesan array integer 3 elemen
         cout<<data.at(5)<<endl;
         //memanggil array ke elemen 5
-
     }
+    catch(exception& e) {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /* akan dieksekusi karena array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+    /* penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
