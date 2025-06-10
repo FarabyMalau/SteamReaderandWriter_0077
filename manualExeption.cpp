0fd607d11b4205c5a2484701fb571eac5acc28e2
FarabyMalau <farabymuzakki49@gmail.com>
2025-06-10 15:19:45 +0600

    Menambahkan try

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,4 +1,12 @@
-#include <fstream>
 #include <iostream>
-#include <string>
 using namespace std;
+
+int main() 
+{
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+    
+}
